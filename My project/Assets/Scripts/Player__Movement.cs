830a6e3b139129e348cbcccc1e1cc4ba5154bc32
Postolache Andreea Miruna <79594745+postolache-andreea-miruna@users.noreply.github.com>
2022-11-05 01:26:38 +0200

    Named diffrent one script

--- My project/Assets/Scripts/Player__Movement.cs
+++ My project/Assets/Scripts/Player__Movement.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
+public class Player__Movement : MonoBehaviour
 {
     private Rigidbody2D _rigidBody;
     private SpriteRenderer _spriteRenderer;
